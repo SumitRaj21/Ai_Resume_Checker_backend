0b2455a8b6a83536c3f8269fa457dac26ff8a650
SumitRaj21 <sumitraj9119@gmail.com>
2026-04-13 17:22:41 +0530

    test

--- backend/utils/asyncHandler.js
+++ backend/utils/asyncHandler.js
@@ -12,6 +12,7 @@ const asyncHandler = (fn) => {
         try {
             await fn(req, res, next);
         } catch (error) {
+            console.log(error);
             next(error);
         }
     };
